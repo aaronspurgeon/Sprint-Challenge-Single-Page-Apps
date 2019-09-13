a2fb4b345e2fb8d10932d4278f8efa8e46f6cc97
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-09-13 19:31:28 -0400

    i have the data for each character displaying correctly

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -2,10 +2,14 @@ import React from "react";
 
 export default function CharacterCard({ character }) {
   console.log('character card props', character)
-  const { name } = character
+  const { name, location, origin, species, status, image } = character
   return (
     <div>
+      <img src={`${image}`} alt={`${name} from Rick and Morty!`}/>
       <h1>{name}</h1>
+      <span>{`${species} ${status}`}</span>
+      <p>Location: {location.name}</p>
+      <p>Origin: {origin.name}</p>
     </div>
   )
 }
