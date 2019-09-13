75156964826847f0d041b4c788785b2574241e5b
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-09-13 19:09:08 -0400

    testing axios call within characterlist compononent

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,11 +1,25 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [character, setCharacter] = useState([]);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    const getCharacter = () => {
+      axios
+        .get('https://rickandmortyapi.com/api/character/')
+        .then(res => {
+          console.log(res)
+        })
+        .catch(error => {
+          console.error('Server Error', error)
+        })
+    }
+
+    getCharacter();
   }, []);
 
   return (
