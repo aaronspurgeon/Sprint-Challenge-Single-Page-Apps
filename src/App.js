95b0476005958df77403dd31f002bed2619de2d0
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-09-13 19:54:35 -0400

    re did the jsx for characters to show a card from semantic ui react

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ export default function App() {
   return (
     <main>
       <Header />
-      {/* <CharacterList /> */}
-      <LocationList />
+      <CharacterList />
+      {/* <LocationList /> */}
       {/* <TabNav /> */}
     </main>
   );
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,15 +1,26 @@
 import React from "react";
+import { Card, Icon, Image } from 'semantic-ui-react'
 
 export default function CharacterCard({ character }) {
   console.log('character card props', character)
   const { name, location, origin, species, status, image } = character
   return (
-    <div>
-      <img src={`${image}`} alt={`${name} from Rick and Morty!`}/>
-      <h1>{name}</h1>
-      <span>{`${species} ${status}`}</span>
-      <p>Location: {location.name}</p>
-      <p>Origin: {origin.name}</p>
-    </div>
+    <Card>
+      <Image src={`${image}`} wrapped ui={false} />
+      <Card.Content>
+        <Card.Header>{name}</Card.Header>
+        <Card.Meta>{`${species} ${status}`}</Card.Meta>
+        <Card.Description>Location: {location.name}</Card.Description>
+        <Card.Description>Origin: {origin.name}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <a>
+          <Icon name='user' />
+          Episodes
+        </a>
+      </Card.Content>
+    </Card>
   )
 }
+
+
