2423d37fa07b835ecd727f44f9d392f383719942
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-09-13 21:45:44 -0400

    mvp complete

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -1,35 +1,37 @@
 import React from "react";
 import { Tab, Menu, Icon } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
-import CharacterList from './CharacterList';
-import LocationsList from './LocationsList';
-import EpisodesList from './EpisodesList';
-import WelcomePage from './WelcomePage';
-
-
-
+import CharacterList from "./CharacterList";
+import LocationsList from "./LocationsList";
+import EpisodesList from "./EpisodesList";
+import WelcomePage from "./WelcomePage";
 // TODO: Add missing menu/tabs/nav below
-
 // Review Semantic UI Component options for nav-like UI:
 // https://react.semantic-ui.com/collections/menu/
 // https://react.semantic-ui.com/modules/tab/
 // https://react.semantic-ui.com/elements/button/
 // https://react.semantic-ui.com/collections/breadcrumb/
-
-    
-    const panes = [
-        { menuItem: 'Home Page', render: () => <Tab.Pane to={'/'} as={NavLink}></Tab.Pane>},
-        { menuItem: 'Characters', render: () => <Tab.Pane to={'/characters'} as={NavLink}></Tab.Pane>},
-        { menuItem: 'Locations', render: () => <Tab.Pane to={'/locations'} as={NavLink}></Tab.Pane>},
-        { menuItem: 'Episodes', render: () => <Tab.Pane to={'/episodes'} as={NavLink}></Tab.Pane>}
-    ]
-
-    const TabNav = () => {
-        return <Tab panes={panes} />     
-        
-}
-
-    export default TabNav;
+const Nav = props => <NavLink exact {...props} activeClassName="active" />;
+const panes = [
+    {
+        menuItem: (
+            <Menu.Item key="home" as={Nav} to={`/`}>
+            Home
+            </Menu.Item>
+        )
+    },
+    {
+        menuItem: <Menu.Item key="characters" as={Nav} to={`/characters`} >Characters</Menu.Item>
+    },
+    {
+        menuItem: <Menu.Item key="locations" as={Nav} to={`/locations`}>Locations</Menu.Item>
+    },
+    {
+        menuItem: <Menu.Item key="episodes" as={Nav} to={`/episodes`}>Episodes</Menu.Item>
+    }
+];
+const TabNav = () => <Tab panes={panes} renderActiveOnly={false} />;
+export default TabNav;
 
     
    
